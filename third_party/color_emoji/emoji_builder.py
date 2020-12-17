4d2dc8b20e3aae383b47340dce1ac28e26519586
YPwn <ypwn@ypwn>
2020-12-17 14:51:34 +0000

    Adjust for Android 5 compatibility (really? such a small change prevented it from working?..)

--- third_party/color_emoji/emoji_builder.py
+++ third_party/color_emoji/emoji_builder.py
@@ -82,7 +82,7 @@ class CBDT:
 		return self.stream
 
 	def write_header (self):
-		self.write (struct.pack (">L", 0x00030000)) # FIXED version
+		self.write (struct.pack (">L", 0x00020000)) # FIXED version
 
 	def start_strike (self, strike_metrics):
 		self.strike_metrics = strike_metrics
@@ -239,7 +239,7 @@ class CBLC:
 		return stream
 
 	def write_header (self):
-		self.write (struct.pack (">L", 0x00030000)) # FIXED version
+		self.write (struct.pack (">L", 0x00020000)) # FIXED version
 
 	def start_strikes (self, num_strikes):
 		self.num_strikes = num_strikes
